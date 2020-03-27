d71febb77395377641c34ca7b9bc623080078519
Andrey Manakhov <andreibikerrr@gmail.com>
2020-03-27 11:05:05 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+  let strNum = n.toString();
+  let result = '';
+
+  for (let i = strNum.length-1; i>=0; i--) {
+      if (strNum[i] === '-') continue;
+      result += strNum[i];
+  }
+
+  result = + result;
+
+  return result;
 }
